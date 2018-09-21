81dbdf0f3d7d85d8b1cd8183d45bfe4ee288d77c
leitao1 <2534545316@qq.com>
2018-09-21 10:15:44 +0800

    init

---
+++ src/main/java/com/winxuan/elasticsearch/ApplicationRunner.java
@@ -0,0 +1,19 @@
+package com.winxuan.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leitao.
+ * @category
+ * @time: 2018/9/21 0021-10:07
+ * @version: 1.0
+ * @description:
+ **/
+@SpringBootApplication
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
